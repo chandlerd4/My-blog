1c6db88cf662d613fa74e3122d895f68337fd660
chan <chandlerdaniel254@gmail.com>
2024-06-09 14:27:26 -0400

    added local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,3 +12,16 @@ else{
     event.target.textContent="dark"
 }
 })
+var formEntry=document.getElementById("form-entry")
+formEntry.addEventListener("submit", function(event){
+    event.preventDefault()
+    var blogList= JSON.parse(localStorage.getItem("personalBlog")) || []
+    var userData= {
+        title:document.getElementById("title").value,
+        description:document.getElementById("description").value,
+        username:document.getElementById("username").value
+    }
+    blogList.push(userData)
+    console.log(blogList)
+    localStorage.setItem("personalBlog",JSON.stringify(blogList))
+})
